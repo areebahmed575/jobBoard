681b4531c8823a39abf910fe4077627887935387
areebahmed575 <areebahmed575@gmail.com>
2023-10-26 04:02:24 -0600

    fix some deployment  errors

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,10 +1,3 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth({
-    onError(req, res, error) {
-      console.log(error);
-      console.log(req)
-      res.status(error.status || 500).end('Check the console for the error');
-    }
-  });
-  
+export const GET = handleAuth();
